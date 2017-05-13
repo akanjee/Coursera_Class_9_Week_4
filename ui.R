602db1a7a6a5e8cda3a8bb0f9ee762023cea76d9
akanjee <amyn.kanjee@gmail.com>
2017-05-13 16:14:06 -0700

    Add files via upload

--- ui.R
+++ ui.R
@@ -2,31 +2,46 @@
 ### Data Products - Week 4 Assignment - UI.R
 ### Code for Simple Plotting Function
 ### Code based on sample provided at http://shiny.rstudio.com
-###
+### Date: May 13/2017
+### Produced By: Amyn Kanjee
 ######
 
-shinyUI(bootstrapPage(
+
+shinyUI(fluidPage
+        (
     
-                
-    sliderInput(inputId = "n_breaks",
-                label = "Number of bins in histogram (approximate):",
-                min = 1, max = 20, value = 10, step = 1),
+            titlePanel("MT Car Shiny App"),
     
-    checkboxInput(inputId = "individual_obs",
-                  label = strong("Show individual observations"),
-                  value = FALSE),
+            sidebarLayout(
+                sidebarPanel(
+                    
+                    sliderInput(inputId = "n_breaks",
+                                label = "1. Select the number of of bins for the histogram:",
+                                min = 1, max = 20, value = 10, step = 1),
+                    
+                    checkboxInput(inputId = "individual_obs",
+                                  label = ("2. Choose whether or not you want to show the individual observations on the chart"),
+                                  value = FALSE),
+                    
+                    checkboxInput(inputId = "density",
+                                  label = ("3. Choose whether not to show the density estimate on the chart"),
+                                  value = FALSE)
+                    
+                ),
+        
+                mainPanel(
+            
     
-    checkboxInput(inputId = "density",
-                  label = strong("Show density estimate"),
-                  value = FALSE),
+                    plotOutput(outputId = "main_plot", height = "300px"),
     
-    plotOutput(outputId = "main_plot", height = "300px"),
-    
-    # Display this only if the density is shown
-    conditionalPanel(condition = "input.density == true",
+                # Display this only if the density is shown
+                conditionalPanel(condition = "input.density == true",
                      sliderInput(inputId = "bw_adjust",
-                                 label = "Bandwidth adjustment:",
+                                 label = "4. Adjust the bandwidth as required",
                                  min = 0.2, max = 2, value = 1, step = 0.2)
-    )
+                     )
     
-))
+            )
+        )
+    )
+)
